dec933da7099634c58b3abc188dddca6a3122eb0
shayanmukhtar <shayan.mukhtar@gmail.com>
2017-03-15 22:14:45 -0400

    Investigate tasking on Fibonnaci sequence

--- OpenMP_Fib/src/OpenMP_Fib.c
+++ OpenMP_Fib/src/OpenMP_Fib.c
@@ -12,9 +12,13 @@
 #include <stdio.h>
 #include <omp.h>
 
+int Fibonacci(int sequenceLength);
+
 int main(int argc, char *argv[])
 {
+	//test parallel threads are building with openMP
 	int num_threads = 0;// = omp_get_num_threads();
+	int fibonacciSequence;
 	#pragma omp parallel
 	{
 		int thread_id = omp_get_thread_num();
@@ -26,6 +30,29 @@ int main(int argc, char *argv[])
 		#pragma omp barrier
 		printf("Hello World from thread %d of %d\n",thread_id, num_threads);
 
+
+
+	fibonacciSequence = Fibonacci(10);
 	}     //omp parallel
+	printf("Fibonacci returns %d\n", fibonacciSequence);
 	return(0);
+}
+
+int Fibonacci(int sequenceLength)
+{
+	int returnValue, x, y;
+	if (sequenceLength < 2)
+	{
+		returnValue = sequenceLength;
+	}
+	else
+	{
+#pragma omp task shared(x)
+		x = (sequenceLength - 1);
+#pragma omp task shared(y)
+		y = (sequenceLength - 2);
+#pragma omp taskwait
+		returnValue = x + y;
+	}
+	return returnValue;
 }
